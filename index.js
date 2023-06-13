abbb040a1784f6f7e13a333f0fe42d8bff01d0ae
rushikesh <rushikeshchopde8086@gmail.com>
2023-06-13 23:39:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const dotenv = require("dotenv")
+dotenv.config({ path: './config.env' })
+const port = process.env.PORT
+const mongoDB = require("./db")
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+mongoDB();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.use(express.json());
+
+app.use('/api', require("./Routes/CreatUser"));
+
+app.use('/api', require("./Routes/DisplayData"));
+app.use('/api', require("./Routes/OrderData"));
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
