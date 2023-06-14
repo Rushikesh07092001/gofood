4454911239f6f5ea723dddf0071efde01acafa46
rushikesh <rushikeshchopde8086@gmail.com>
2023-06-14 18:45:27 +0530

    six commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT
 const mongoDB = require("./db")
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", "https://gofoodbackend-rlkd.onrender.com");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
