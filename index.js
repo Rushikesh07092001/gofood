ff6e3a313abf79312633575f6b363c1196a2b777
rushikesh <rushikeshchopde8086@gmail.com>
2023-06-16 20:40:39 +0530

    second commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const mongoDB = require("./db")
     // const path = require('path')
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", "https://gofoodapp.netlify.app");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
